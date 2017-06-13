b0833da85a9feae1b2d5e3867fab06b51890d930
celloalex <alexbuchaniec@gmail.com>
2017-06-13 18:34:14 -0500

    Add project files.

---
+++ E-commerce Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_commerce_Project.Startup))]
+namespace E_commerce_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
