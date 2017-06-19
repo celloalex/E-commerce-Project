c197cdf6d71434ea0cab92751dc81cdaf430cacf
celloalex <alexbuchaniec@gmail.com>
2017-06-19 14:08:59 -0500

    Added completed date

--- ECommerceDB/dbo/Tables/Purchase.sql
+++ ECommerceDB/dbo/Tables/Purchase.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Purchase] (
     [ID]         INT IDENTITY (1, 1) NOT NULL,
     [CustomerID] INT NOT NULL,
+    [CompletedDate] DATETIME NULL, 
     CONSTRAINT [PK_Purchase] PRIMARY KEY CLUSTERED ([ID] ASC),
 	CONSTRAINT [FK_Purchase_Customer] FOREIGN KEY (CustomerID) REFERENCES Customer(Id)
 );
